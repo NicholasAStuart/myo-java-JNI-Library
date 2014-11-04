bcfc5fe2d8ac4cde6461f0fe61da35b4e2e38e2b
voidplus <mail@voidplus.de>
2014-11-05 00:49:52 +0100

    added local directory

--- compile.sh
+++ compile.sh
@@ -3,15 +3,15 @@ export JAVA_HOME=`/usr/libexec/java_home -v 1.7`
 g++ -arch x86_64 -arch i386 -c \
 -I $JAVA_HOME/include \
 -I $JAVA_HOME/include/darwin \
--F $MYO_HOME \
+-F $MYO_HOME . \
 	com_thalmic_myo_Myo.cpp \
 	com_thalmic_myo_Hub.cpp \
 	handle.cpp \
-	jnidevicelistener.cpp 
+	jnidevicelistener.cpp
 
 g++ -dynamiclib -arch x86_64 -arch i386 \
 -Wl,-rpath,@loader_path \
--F $MYO_HOME -framework myo -framework JavaVM -o libmyo.jnilib \
+-F $MYO_HOME . -framework myo -framework JavaVM -o libmyo.jnilib \
 	com_thalmic_myo_Myo.o \
 	com_thalmic_myo_Hub.o \
 	handle.o \
